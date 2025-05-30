68f486025dae84c774f268bd0175238f18069972
Adarsh <adarsh80810@gmail.com>
2025-05-30 17:26:45 +0530

    add these modified files to my repositry

--- src/Sortingstring.java
+++ src/Sortingstring.java
@@ -5,16 +5,20 @@ import java.util.List;
 
 public class Sortingstring {
     public static void main(String[] args) {
+        System.out.println(countingFrequency());
 
     }
-    public static String[] sort(){
-        List<String> myList = new ArrayList<>();
-        myList.add("1");
-        myList.add("2");
-        myList.add("3");
+   public static int  countingFrequency(){
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+       myList.add(3);
+        myList.add(4);
+        myList.add(5);
+       return Collections.frequency(myList,5);
 
-        return myList;
-    }
+   }
+
 
 
 }
